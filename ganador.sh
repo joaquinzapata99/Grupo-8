734aeca29e2e752d6830f6dc43c20006e81db6e9
Maurobalas <maurobal@hotmail.es>
2024-10-07 18:59:06 +0200

    4

--- ganador.sh
+++ ganador.sh
@@ -1,14 +1,13 @@
-### 4 SI AHORA LOS MINUTOS SON PAR, DECIR "HAS GANADO" GUARDAR FICHERO COMO GANADOR.TXT
-
 #!/bin/bash
-
-rm ganador.txt
-
-# Obtener el minuto actual
-minuto=$(date +"%M")
+### 4 SI AHORA LOS MINUTOS SON PAR, DECIR "HAS GANADO" GUARDAR FICHERO COMO GANADOR.TXT
 
 # Comprobar si el minuto es par
 if [ $((minuto % 2)) -eq 0 ]; then
-    echo "HAS GANADO"
-    echo "HAS GANADO" > ganador.txt
+    
+    # Obtener la fecha y hora actual
+    fecha=$(date +"%Y-%m-%d %H:%M:%S")
+    
+    # Mostrar y guardar el mensaje en ganador.txt dentro de la carpeta
+    echo "HAS GANADO, "
+    echo "HAS GANADO - $fecha" > "$1/ganador.txt"
 fi
